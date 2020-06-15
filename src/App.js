5a76a67c697d4928d94eafc6928ed460b65e6018
Andrey <a.so777@live.ca>
2020-06-15 12:08:56 -0700

    Added controls to the UI
    Implemented zoom, rectangle, redact and select

--- src/App.js
+++ src/App.js
@@ -1,14 +1,17 @@
-import React, { useRef, useEffect } from 'react';
+import React, { useRef, useEffect, useState } from 'react';
+import Header from './components/Header';
 import './App.css';
 
 const App = () => {
   const viewer = useRef(null);
   const scrollView = useRef(null);
+  const [docViewer, setDocViewer] = useState(null);
 
   // if using a class, equivalent of componentDidMount
   useEffect(() => {
     const CoreControls = window.CoreControls;
     CoreControls.setWorkerPath('/webviewer');
+    CoreControls.enableFullPDF(true);
 
     const docViewer = new CoreControls.DocumentViewer();
     docViewer.setScrollViewElement(scrollView.current);
@@ -16,16 +19,52 @@ const App = () => {
     docViewer.setOptions({ enableAnnotations: true });
     docViewer.loadDocument('/files/pdftron_about.pdf');
 
+    console.log(docViewer);
+
+    setDocViewer(docViewer);
+
     docViewer.on('documentLoaded', () => {
       console.log('document loaded');
-      // enable default tool for text and annotation selection
       docViewer.setToolMode(docViewer.getTool('AnnotationEdit'));
     });
   }, []);
 
+  const zoomOut = () => {
+    docViewer.zoomTo(docViewer.getZoom() - 0.25);
+  };
+
+  const zoomIn = () => {
+    docViewer.zoomTo(docViewer.getZoom() + 0.25);
+  };
+
+  const createRectangle = () => {
+    docViewer.setToolMode(docViewer.getTool('AnnotationCreateRectangle'));
+  };
+
+  const selectTool = () => {
+    docViewer.setToolMode(docViewer.getTool('AnnotationEdit'));
+  };
+
+  const createRedaction = () => {
+    docViewer.setToolMode(docViewer.getTool('AnnotationCreateRedaction'));
+  };
+
+  const applyRedactions = async () => {
+    const annotManager = docViewer.getAnnotationManager();
+    annotManager.enableRedaction(true);
+    await annotManager.applyRedactions();
+  };
+
   return (
     <div className="App">
-      <div className="header">React sample</div>
+      <div>
+        <button onClick={zoomOut}>Zoom out</button>
+        <button onClick={zoomIn}>Zoom in</button>
+        <button onClick={createRectangle}>Rectangle</button>
+        <button onClick={createRedaction}>Redact</button>
+        <button onClick={applyRedactions}>Apply Redactions</button>
+        <button onClick={selectTool}>Select</button>
+      </div>
       <div id="scroll-view" ref={scrollView}>
         <div id="viewer" ref={viewer}></div>
       </div>
