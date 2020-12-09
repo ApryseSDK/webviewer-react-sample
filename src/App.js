27cb7e8f94015c7e156341fca69c7e2e16b941d2
Andrey Safonov <a.so777@live.ca>
2020-12-09 14:55:57 -0800

    Added webviewer-html

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { useRef, useEffect } from 'react';
 import WebViewer from '@pdftron/webviewer';
+import { initializeHTMLViewer } from '@pdftron/webviewer-html';
 import './App.css';
 
 const App = () => {
@@ -13,24 +14,25 @@ const App = () => {
         initialDoc: '/files/PDFTRON_about.pdf',
       },
       viewer.current,
-    ).then((instance) => {
-      const { docViewer, Annotations } = instance;
-      const annotManager = docViewer.getAnnotationManager();
+    ).then(async (instance) => {
 
-      docViewer.on('documentLoaded', () => {
-        const rectangleAnnot = new Annotations.RectangleAnnotation();
-        rectangleAnnot.PageNumber = 1;
-        // values are in page coordinates with (0, 0) in the top left
-        rectangleAnnot.X = 100;
-        rectangleAnnot.Y = 150;
-        rectangleAnnot.Width = 200;
-        rectangleAnnot.Height = 50;
-        rectangleAnnot.Author = annotManager.getCurrentUser();
+      const { loadHTMLPage, loadHTMLPages, toggleAnnotations } = await initializeHTMLViewer(instance);
 
-        annotManager.addAnnotation(rectangleAnnot);
-        // need to draw the annotation otherwise it won't show up until the page is refreshed
-        annotManager.redrawAnnotation(rectangleAnnot);
-      });
+      loadHTMLPages([
+        {
+          url:
+            'https://www.google.com/maps/embed?pb=!1m14!1m12!1m3!1d41656.714956835!2d-123.0850416!3d49.26607539999999!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!5e0!3m2!1sen!2sca!4v1591640645684!5m2!1sen!2sca',
+          width: 1800,
+          height: 1100,
+        },
+        {
+          url:
+            'https://www.openstreetmap.org/export/embed.html?bbox=-0.004017949104309083%2C51.47612752641776%2C0.00030577182769775396%2C51.478569861898606&layer=mapnik',
+          width: 600,
+          height: 450,
+        },
+      ]);
+      
     });
   }, []);
 
