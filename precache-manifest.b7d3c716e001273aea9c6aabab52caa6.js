4db69d7b9d3c0cb828afbde1ae6fd8394cc96d79
Andrey Safonov <a.so777@live.ca>
2021-06-21 14:22:05 -0700

    Updates

---
+++ precache-manifest.b7d3c716e001273aea9c6aabab52caa6.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "dd9d4c596cbf22274865",
+    "url": "/webviewer-react-sample/static/css/main.19e75dfd.chunk.css"
+  },
+  {
+    "revision": "dd9d4c596cbf22274865",
+    "url": "/webviewer-react-sample/static/js/main.dd9d4c59.chunk.js"
+  },
+  {
+    "revision": "78d43f1a99c80cd94a28",
+    "url": "/webviewer-react-sample/static/js/1.78d43f1a.chunk.js"
+  },
+  {
+    "revision": "23dd03fbedbe42b205ef",
+    "url": "/webviewer-react-sample/static/js/runtime~main.23dd03fb.js"
+  },
+  {
+    "revision": "dcd3dcaf8e9ca44794207d275a3a55f7",
+    "url": "/webviewer-react-sample/index.html"
+  }
+];
