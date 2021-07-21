e7e0f0e83c4cb851d80a2069145967b798b900fa
Matt Parizeau <mparizeau@pdftron.com>
2021-07-20 17:50:44 -0700

    Update to use WebViewer 8.0

--- src/App.js
+++ src/App.js
@@ -14,22 +14,22 @@ const App = () => {
       },
       viewer.current,
     ).then((instance) => {
-      const { docViewer, Annotations } = instance;
-      const annotManager = docViewer.getAnnotationManager();
+      const { documentViewer, annotationManager, Annotations } = instance.Core;
 
-      docViewer.on('documentLoaded', () => {
-        const rectangleAnnot = new Annotations.RectangleAnnotation();
-        rectangleAnnot.PageNumber = 1;
-        // values are in page coordinates with (0, 0) in the top left
-        rectangleAnnot.X = 100;
-        rectangleAnnot.Y = 150;
-        rectangleAnnot.Width = 200;
-        rectangleAnnot.Height = 50;
-        rectangleAnnot.Author = annotManager.getCurrentUser();
+      documentViewer.addEventListener('documentLoaded', () => {
+        const rectangleAnnot = new Annotations.RectangleAnnotation({
+          PageNumber: 1,
+          // values are in page coordinates with (0, 0) in the top left
+          X: 100,
+          Y: 150,
+          Width: 200,
+          Height: 50,
+          Author: annotationManager.getCurrentUser()
+        });
 
-        annotManager.addAnnotation(rectangleAnnot);
+        annotationManager.addAnnotation(rectangleAnnot);
         // need to draw the annotation otherwise it won't show up until the page is refreshed
-        annotManager.redrawAnnotation(rectangleAnnot);
+        annotationManager.redrawAnnotation(rectangleAnnot);
       });
     });
   }, []);
