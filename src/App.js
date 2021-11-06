6b3e4fac8c913078a504bc6523eac7f1cc284871
Andrey Safonov <a.so777@live.ca>
2021-11-05 21:25:14 -0700

    Added document template generation sample

--- src/App.js
+++ src/App.js
@@ -5,39 +5,55 @@ import './App.css';
 const App = () => {
   const viewer = useRef(null);
 
-  // if using a class, equivalent of componentDidMount 
+  const jsonData = {
+    COMPANYNAME: 'PDFTron',
+    CUSTOMERNAME: 'Andrey Safonov',
+    CompanyAddressLine1: '838 W Hastings St 5th floor',
+    CompanyAddressLine2: 'Vancouver, BC V6C 0A6',
+    CustomerAddressLine1: '123 Main Street',
+    CustomerAddressLine2: 'Vancouver, BC V6A 2S5',
+    Date: 'Nov 5th, 2021',
+    ExpiryDate: 'Dec 5th, 2021',
+    QuoteNumber: '134',
+    WEBSITE: 'www.pdftron.com',
+    billed_items: {
+      insert_rows: [
+        ['Apples', '3', '$5.00', '$15.00'],
+        ['Oranges', '2', '$5.00', '$10.00'],
+      ],
+    },
+    days: '30',
+    total: '$25.00',
+  };
+
+  // if using a class, equivalent of componentDidMount
   useEffect(() => {
     WebViewer(
       {
         path: '/webviewer/lib',
-        initialDoc: '/files/PDFTRON_about.pdf',
+        initialDoc: '/files/quote.docx',
       },
-      viewer.current,
-    ).then((instance) => {
-      const { documentViewer, annotationManager, Annotations } = instance.Core;
+      viewer.current
+    ).then(async (instance) => {
+      const { documentViewer } = instance.Core;
 
-      documentViewer.addEventListener('documentLoaded', () => {
-        const rectangleAnnot = new Annotations.RectangleAnnotation({
-          PageNumber: 1,
-          // values are in page coordinates with (0, 0) in the top left
-          X: 100,
-          Y: 150,
-          Width: 200,
-          Height: 50,
-          Author: annotationManager.getCurrentUser()
-        });
+      documentViewer.addEventListener('documentLoaded', async () => {
+        await documentViewer.getDocument().documentCompletePromise();
+        documentViewer.updateView();
 
-        annotationManager.addAnnotation(rectangleAnnot);
-        // need to draw the annotation otherwise it won't show up until the page is refreshed
-        annotationManager.redrawAnnotation(rectangleAnnot);
+        // const doc = documentViewer.getDocument();
+        // const keys = doc.getTemplateKeys();
+        // console.log(keys);
+
+        await documentViewer.getDocument().applyTemplateValues(jsonData);
       });
     });
   }, []);
 
   return (
-    <div className="App">
-      <div className="header">React sample</div>
-      <div className="webviewer" ref={viewer}></div>
+    <div className='App'>
+      <div className='header'>React sample</div>
+      <div className='webviewer' ref={viewer}></div>
     </div>
   );
 };
