635ade15703785df449214be26f6ecb4a09e4c88
Andrey Safonov <a.so777@live.ca>
2022-03-15 10:29:29 -0700

    initial commit

--- src/App.js
+++ src/App.js
@@ -10,26 +10,24 @@ const App = () => {
     WebViewer(
       {
         path: '/webviewer/lib',
-        initialDoc: '/files/PDFTRON_about.pdf',
+        fullAPI: true,
       },
       viewer.current,
-    ).then((instance) => {
-      const { documentViewer, annotationManager, Annotations } = instance.Core;
+    ).then(async (instance) => {
+      const { Core, UI } = instance;
+      const { PDFNet } = Core;
+      
+      await PDFNet.initialize();
 
-      documentViewer.addEventListener('documentLoaded', () => {
-        const rectangleAnnot = new Annotations.RectangleAnnotation({
-          PageNumber: 1,
-          // values are in page coordinates with (0, 0) in the top left
-          X: 100,
-          Y: 150,
-          Width: 200,
-          Height: 50,
-          Author: annotationManager.getCurrentUser()
-        });
-
-        annotationManager.addAnnotation(rectangleAnnot);
-        // need to draw the annotation otherwise it won't show up until the page is refreshed
-        annotationManager.redrawAnnotation(rectangleAnnot);
+      await PDFNet.runWithoutCleanup(async () => { 
+          const doc = await PDFNet.PDFDoc.create();
+          doc.initSecurityHandler();
+          doc.lock();
+  
+          const tiffFile = await PDFNet.Filter.createURLFilter("./files/test.tif");
+          await PDFNet.Convert.fromTiff(doc, tiffFile);
+          doc.unlock();
+          UI.loadDocument(doc);
       });
     });
   }, []);
