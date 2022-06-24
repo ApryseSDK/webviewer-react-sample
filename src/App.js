1b16fe7e4ede9995611e9de1e1845fd2213884cb
Kristian Hein <kristianhein@gmail.com>
2022-06-24 15:43:37 -0700

    initial

--- src/App.js
+++ src/App.js
@@ -10,33 +10,20 @@ const App = () => {
     WebViewer(
       {
         path: '/webviewer/lib',
-        initialDoc: '/files/PDFTRON_about.pdf',
+        initialDoc: '/files/outlines-nested.pdf',
       },
       viewer.current,
     ).then((instance) => {
-      const { docViewer, Annotations } = instance;
-      const annotManager = docViewer.getAnnotationManager();
-
-      docViewer.on('documentLoaded', () => {
-        const rectangleAnnot = new Annotations.RectangleAnnotation();
-        rectangleAnnot.PageNumber = 1;
-        // values are in page coordinates with (0, 0) in the top left
-        rectangleAnnot.X = 100;
-        rectangleAnnot.Y = 150;
-        rectangleAnnot.Width = 200;
-        rectangleAnnot.Height = 50;
-        rectangleAnnot.Author = annotManager.getCurrentUser();
-
-        annotManager.addAnnotation(rectangleAnnot);
-        // need to draw the annotation otherwise it won't show up until the page is refreshed
-        annotManager.redrawAnnotation(rectangleAnnot);
-      });
+      instance.UI.enableElements([
+        'bookmarksPanel',
+        'bookmarksPanelButton',
+      ]);
     });
   }, []);
 
   return (
     <div className="App">
-      <div className="header">React sample</div>
+      {/* <div className="header">React sample</div> */}
       <div className="webviewer" ref={viewer}></div>
     </div>
   );
