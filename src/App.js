b719e1f2aabad44b45331556ab6436facca74d42
Andrey Safonov <a.so777@live.ca>
2022-12-20 11:27:32 -0700

    Initial commit

--- src/App.js
+++ src/App.js
@@ -11,33 +11,47 @@ const App = () => {
       {
         path: '/webviewer/lib',
         initialDoc: '/files/PDFTRON_about.pdf',
+        disabledElements: [
+          'header',
+          'toolsHeader',
+          'pageNavOverlay'
+        ]
       },
       viewer.current,
     ).then((instance) => {
-      const { documentViewer, annotationManager, Annotations } = instance.Core;
+      const { Core, UI } = instance;
+      const { documentViewer, annotationManager, Annotations } = Core;
+      const LayoutMode = UI.LayoutMode;
 
-      documentViewer.addEventListener('documentLoaded', () => {
-        const rectangleAnnot = new Annotations.RectangleAnnotation({
-          PageNumber: 1,
-          // values are in page coordinates with (0, 0) in the top left
-          X: 100,
-          Y: 150,
-          Width: 200,
-          Height: 50,
-          Author: annotationManager.getCurrentUser()
-        });
 
-        annotationManager.addAnnotation(rectangleAnnot);
-        // need to draw the annotation otherwise it won't show up until the page is refreshed
-        annotationManager.redrawAnnotation(rectangleAnnot);
+      documentViewer.addEventListener('documentLoaded', function() {
+        UI.setFitMode(UI.FitMode.FitWidth);
+        UI.setLayoutMode(LayoutMode.Single);
       });
+
+      document.getElementById('prevButton').onclick = e => {
+        if (documentViewer.getCurrentPage() - 1 > 0) {
+          documentViewer.setCurrentPage(
+            Math.max(documentViewer.getCurrentPage() - 1, 1),
+          );
+        }
+      };
+
+      document.getElementById('nextButton').onclick = e => {
+        if (documentViewer.getCurrentPage() + 1 <= documentViewer.getPageCount()) {
+          documentViewer.setCurrentPage(
+            Math.min(documentViewer.getCurrentPage() + 1, documentViewer.getPageCount())
+          );
+        }
+      };
     });
   }, []);
 
   return (
     <div className="App">
-      <div className="header">React sample</div>
+      <button type="button" id="prevButton">Previous</button>
       <div className="webviewer" ref={viewer}></div>
+      <button type="button" id="nextButton">Next</button>
     </div>
   );
 };
