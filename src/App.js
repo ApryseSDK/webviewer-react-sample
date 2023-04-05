61e6cd6cb79b187677efce11abddcb074c636c8f
maggievu <mvu@pdftron.com>
2023-04-05 13:08:54 -0700

    Test loading time for v10

--- src/App.js
+++ src/App.js
@@ -14,22 +14,9 @@ const App = () => {
       },
       viewer.current,
     ).then((instance) => {
-      const { documentViewer, annotationManager, Annotations } = instance.Core;
-
-      documentViewer.addEventListener('documentLoaded', () => {
-        const rectangleAnnot = new Annotations.RectangleAnnotation({
-          PageNumber: 1,
-          // values are in page coordinates with (0, 0) in the top left
-          X: 100,
-          Y: 150,
-          Width: 200,
-          Height: 50,
-          Author: annotationManager.getCurrentUser()
-        });
-
-        annotationManager.addAnnotation(rectangleAnnot);
-        // need to draw the annotation otherwise it won't show up until the page is refreshed
-        annotationManager.redrawAnnotation(rectangleAnnot);
+      console.time('v10build');
+      instance.Core.documentViewer.addEventListener('documentLoaded', () => {
+        console.timeEnd('v10build');
       });
     });
   }, []);
