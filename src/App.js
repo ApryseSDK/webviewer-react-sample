5acc68c3f9279d31481c3b3160bd3c6a6bd6a449
maggievu <mvu@pdftron.com>
2023-04-05 15:56:39 -0700

    add timer to DOM

--- src/App.js
+++ src/App.js
@@ -16,13 +16,18 @@ const App = () => {
     ).then((instance) => {
       instance.Core.documentViewer.addEventListener('documentLoaded', () => {
         console.timeEnd('v10build');
+        const end = window.performance.now();
+        window.document.querySelector('#timer').innerText = ` ${end - window.start}ms`;
       });
     });
   }, []);
 
   return (
     <div className="App">
-      <div className="header">React sample</div>
+      <div className="header">
+        WV loading time
+        <span id="timer"></span>
+      </div>
       <div className="webviewer" ref={viewer}></div>
     </div>
   );
