1c014eef4188f83f0f3cfb1f8ad354ade3f501e7
maggievu <mvu@pdftron.com>
2023-11-03 16:50:50 -0700

    init for wvr-3680

--- src/App.js
+++ src/App.js
@@ -10,34 +10,39 @@ const App = () => {
     WebViewer(
       {
         path: '/webviewer/lib',
-        initialDoc: '/files/PDFTRON_about.pdf',
-        licenseKey: 'your_license_key'  // sign up to get a free trial key at https://dev.apryse.com
+        // initialDoc: '/files/0370010-0-0540.DGN.pdf',
+        initialDoc: '/files/007611-009H002.dwg.pdf',
       },
       viewer.current,
-    ).then((instance) => {
-      const { documentViewer, annotationManager, Annotations } = instance.Core;
+    ).then(async (instance) => {
+      window.localStorage.removeItem('init_timestamp');
 
-      documentViewer.addEventListener('documentLoaded', () => {
-        const rectangleAnnot = new Annotations.RectangleAnnotation({
-          PageNumber: 1,
-          // values are in page coordinates with (0, 0) in the top left
-          X: 100,
-          Y: 150,
-          Width: 200,
-          Height: 50,
-          Author: annotationManager.getCurrentUser()
-        });
+      instance.UI.disableElements(["header", "toolsHeader"]);
+      instance.UI.setDefaultPrintOptions({ maintainPageOrientation: true });
 
-        annotationManager.addAnnotation(rectangleAnnot);
-        // need to draw the annotation otherwise it won't show up until the page is refreshed
-        annotationManager.redrawAnnotation(rectangleAnnot);
-      });
+      document.getElementById("fitPageBtn").onclick = (e) => {
+        instance.UI.setFitMode(instance.UI.FitMode.FitPage);
+      };
+
+      document.getElementById("fitWidthBtn").onclick = (e) => {
+        instance.UI.setFitMode(instance.UI.FitMode.FitWidth);
+      };
+
+      document.getElementById("printBtn").onclick = (e) => {
+        instance.UI.openElements(["printModal"]);
+      };
+
     });
   }, []);
 
   return (
     <div className="App">
-      <div className="header">React sample</div>
+      <div className="header">
+        React sample
+        <button id="fitPageBtn">Fit Page</button>
+        <button id="fitWidthBtn">Fit Width</button>
+        <button id="printBtn">Print</button>
+      </div>
       <div className="webviewer" ref={viewer}></div>
     </div>
   );
