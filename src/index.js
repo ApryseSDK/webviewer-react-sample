1fa46d0fe5bbf9ad148bc3bbf4a794a8a87c58a9
Armando DBG <bollain@gmail.com>
2023-11-17 17:53:14 -0800

    Add back strict mode

--- src/index.js
+++ src/index.js
@@ -5,7 +5,11 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(<App />);
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+  );
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
