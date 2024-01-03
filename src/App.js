400e42fbf17f22a200ae887901e3fadb6e0c2866
Parth Patel <parth1755@gmail.com>
2024-01-03 19:36:33 +0530

    Annotation delete issue

--- src/App.js
+++ src/App.js
@@ -5,13 +5,13 @@ import './App.css';
 const App = () => {
   const viewer = useRef(null);
 
-  // if using a class, equivalent of componentDidMount 
+  // if using a class, equivalent of componentDidMount
   useEffect(() => {
     // If you prefer to use the Iframe implementation, you can replace this line with: WebViewer.Iframe(...)
     WebViewer.WebComponent(
       {
         path: '/webviewer/lib',
-        initialDoc: '/files/PDFTRON_about.pdf',
+        initialDoc: '/files/sales-invoice.pdf',
         licenseKey: 'your_license_key',  // sign up to get a free trial key at https://dev.apryse.com
       },
       viewer.current,
