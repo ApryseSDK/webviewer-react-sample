fbd36f92530a9690093a13efed1a8097d0f5e342
Parth Patel <parth1755@gmail.com>
2024-01-19 11:43:05 +0530

    Implemented the express server to serve btotli compressed files

---
+++ server.js
@@ -0,0 +1,46 @@
+const express = require('express');
+const path = require('path');
+const fs = require('fs');
+const util = require('util');
+const brotli = require('iltorb');
+
+
+const app = express();
+const port = process.env.PORT || 4000;
+
+app.get('*', (req, res, next) => {
+  const filePath = path.join(__dirname, 'build', req.path);
+  if(filePath.includes("InfixServerWasm") && filePath.includes("br")) {
+    const read = util.promisify(fs.readFile);
+    read(filePath)
+      .then((content) => {
+        const options = { mode: 0 };
+        brotli.compress(content, options, (err, result) => {
+          if (err) {
+            next(err);
+          } else {
+            res.set('Content-Encoding', 'br');
+            res.set('Content-Type', 'application/octet-stream');
+            res.send(content);
+          }
+        });
+      })
+      .catch(next);
+  } else {
+      fs.access(filePath, fs.constants.F_OK, (err) => {
+        if (err) {
+          next();
+        } else {
+          res.sendFile(filePath);
+        }
+      });
+  }
+});
+
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'build', 'index.html'));
+});
+
+app.listen(port, () => {
+  console.log(`Server is running on http://localhost:${port}`);
+});
--- src/App.js
+++ src/App.js
@@ -5,34 +5,19 @@ import './App.css';
 const App = () => {
   const viewer = useRef(null);
 
-  // if using a class, equivalent of componentDidMount 
+  // if using a class, equivalent of componentDidMount
   useEffect(() => {
     // If you prefer to use the Iframe implementation, you can replace this line with: WebViewer.Iframe(...)
     WebViewer.WebComponent(
       {
         path: '/webviewer/lib',
-        initialDoc: '/files/PDFTRON_about.pdf',
+        initialDoc: '/files/apryse.pdf',
         licenseKey: 'your_license_key',  // sign up to get a free trial key at https://dev.apryse.com
       },
       viewer.current,
     ).then((instance) => {
       const { documentViewer, annotationManager, Annotations } = instance.Core;
-
-      documentViewer.addEventListener('documentLoaded', () => {
-        const rectangleAnnot = new Annotations.RectangleAnnotation({
-          PageNumber: 1,
-          // values are in page coordinates with (0, 0) in the top left
-          X: 100,
-          Y: 150,
-          Width: 200,
-          Height: 50,
-          Author: annotationManager.getCurrentUser()
-        });
-
-        annotationManager.addAnnotation(rectangleAnnot);
-        // need to draw the annotation otherwise it won't show up until the page is refreshed
-        annotationManager.redrawAnnotation(rectangleAnnot);
-      });
+      instance.UI.enableFeatures([instance.UI.Feature.ContentEdit]);
     });
   }, []);
 
