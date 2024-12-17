94a870f78ebc71bd8979c6d620265631266f0327
dandara-navarro <dnavarro@apryse.com>
2024-12-16 20:19:08 -0800

    Migrating Create React App Project to Vite Project

--- src/App.test.jsx
+++ src/App.test.jsx
@@ -1,6 +1,13 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
+// Mock fetch globally prventing the error by intercepting all fetch calls and returning mock data.
+global.fetch = vi.fn((url) =>
+  Promise.resolve({
+    text: () => Promise.resolve(`Mocked fetch content for URL: ${url}`),
+  })
+);
+
 test('renders react sample', () => {
   render(<App />);
   const linkElement = screen.getByText(/react sample/i);
